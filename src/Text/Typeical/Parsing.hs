4e95ef22ef01ebe6717881920e7783e0185e2af2
Christian Gram Kalhauge <christian@kalhauge.dk>
2015-10-13 10:31:23 -0700

    Refactored to parseing module

---
+++ src/Text/Typeical/Parsing.hs
@@ -0,0 +1,72 @@
+-- This module holds helpers functions and the other things to help build
+-- parsers.
+{-# LANGUAGE FlexibleContexts #-}
+module Text.Typeical.Parsing where
+
+import           Text.Parsec;
+import           Data.List;
+import           Control.Monad;
+
+type ParserT s m = ParsecT s () m
+
+within :: Stream s m Char => Char -> Char -> ParserT s m String 
+within begin end = char begin *> many1 (noneOf [end]) <* char end
+
+-- | Allow indent, takes a parser which does not accept linebreaks and
+-- returns a new parser that can handle linebreaks, as long as it
+-- is indented. 
+allowIndent :: Stream String m Char => Int -> ParserT String m a -> ParserT String m a
+allowIndent min parser = do 
+    s <- line                      -- parse the initial line
+    rest <- many $ parseIndent min  -- parse possible indents
+    parseFromString parser $ unwords (s:rest)
+
+-- | Parse a single indented line, with a minimum idention. The minimum
+-- indention is removed from the returning string.
+parseIndent :: Stream s m Char => Int -> ParserT s m String
+parseIndent min = try $ count min ws >> line
+
+-- | Parses a line, e.g. all charactors until a endOfLine
+line :: Stream s m Char => ParserT s m String
+line = manyTill anyChar $ try endOfLine
+
+-- | Parse the content of a string, borrowed from Pandoc
+parseFromString :: Monad m => ParserT String m a -> String -> ParserT String m a
+parseFromString parser str = keepContext $ do
+  setInput str
+  result <- parser 
+  spaces >> eof -- Parse until end of string
+  return result
+
+-- | Keeps context of the parser, especially the postion and the input
+keepContext :: Monad m => ParserT s m a -> ParserT s m a
+keepContext parser = do 
+  oldPos <- getPosition
+  oldInput <- getInput
+  retsult <- parser 
+  setInput oldInput
+  setPosition oldPos
+  return retsult
+
+-- | Parse a word 
+wrd :: Stream s m Char => ParserT s m String
+wrd = many1 letter
+
+-- | Parse a nonbreaking whitespace charactor
+ws :: Stream s m Char => ParserT s m Char
+ws = oneOf " \t"
+
+-- | Skip alot of nonbreaking whitespaces
+skipWs :: Stream s m Char => ParserT s m ()
+skipWs = void $ many ws 
+
+-- | inline compinator for skiping spaces
+infixl 4 <.>, .>, <.
+(<.>) :: Stream s m Char => ParserT s m (a -> b) -> ParserT s m a -> ParserT s m b
+p <.> s = p <*> (skipWs *> s)
+
+(.>) :: Stream s m Char => ParserT s m a -> ParserT s m b -> ParserT s m b
+p .> s = p >> skipWs >> s
+
+(<.) :: Stream s m Char => ParserT s m a -> ParserT s m b -> ParserT s m a
+p <. s = p >>= \x -> skipWs >> s >> return x
